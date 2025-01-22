b313b378670d6150682503b8fd29628746c31842
veeravenu19 <143855799+veeravenu19@users.noreply.github.com>
2025-01-22 06:18:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Decision Making in Java/decision-making-in-java.py
@@ -0,0 +1,34 @@
+
+class Solution:
+    def compareNM(self, n : int, m : int) -> str:
+        # code here
+        if n==m:
+            return "equal"
+        elif n<m:
+            return "lesser"
+        elif n>m:
+            return "greater"
+        
+
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        m = int(input())
+        
+        obj = Solution()
+        res = obj.compareNM(n, m)
+        
+        print(res)
+        
+
+        print("~")
+# } Driver Code Ends
